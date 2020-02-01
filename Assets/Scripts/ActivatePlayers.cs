ec5428e315ac73a9dcccb9424db88129dc16d27a
Lachezar Hubanov (SI) <lachezar.hubanov@sigames.com>
2020-02-01 14:22:10 +0000

    added more animations & start game/activate players scripts

---
+++ Assets/Scripts/ActivatePlayers.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+
+public class ActivatePlayers : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject player1;
+
+	[SerializeField]
+	private GameObject player2;
+
+	[SerializeField]
+	private GameObject eventUtilsPrefab;
+
+	[SerializeField]
+	private UIEventForwarder eventForwarder;
+
+	void Start()
+    {
+		if (eventUtilsPrefab)
+		{
+			eventForwarder = eventUtilsPrefab.GetComponent<UIEventForwarder>();
+		}
+
+		if (eventForwarder)
+		{
+			UIEventForwarder.OnForwardedEvent += ActivatePlayersEventHandler;
+		}
+	}
+
+    void Update()
+    {
+    }
+
+	private void ActivatePlayersEventHandler(UIEventArgsBase args)
+	{
+		if (args != null)
+		{
+			Type targetType = args.target;
+			if (targetType == this.GetType())
+			{
+				player1.SetActive(true);
+				player2.SetActive(true);
+			}
+		}
+	}
+}
