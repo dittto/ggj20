aa404d70f0ba0a95283eb34704c457bce76aa1db
dittto <nick@ditt.to>
2020-02-01 22:00:52 +0000

    Stop camera zooming so far in and out

--- Assets/Scripts/CameraZoom.cs
+++ Assets/Scripts/CameraZoom.cs
@@ -7,10 +7,13 @@ public class CameraZoom : MonoBehaviour
 {
     public Camera _camera;
     public List<GameObject> _players;
-    public float _maxBoundary = 0.1f;
-    public float _minBoundary = 0.25f;
-    public float _cameraSpeedOut = 0.2f;
-    public float _cameraSpeedIn = 0.3f;
+    private float _maxerBoundary = 0.05f;
+    private float _maxBoundary = 0.15f;
+    private float _minBoundary = 0.4f;
+    private float _cameraSpeedOuter = 0.2f;
+    private float _cameraSpeedOut = 0.1f;
+    private float _cameraSpeedIn = 0.2f;
+    private float _maxDistance = 15f;
 
     private bool _allowZoom = false;
     
@@ -61,23 +64,35 @@ public class CameraZoom : MonoBehaviour
             return;
         }
 
+        var allIn = true;
+        var totalVector = new Vector3(0, 0, 0);
         foreach (GameObject player in _players) {
             var pos = _camera.WorldToViewportPoint(player.transform.position);
-            if (pos.x < _maxBoundary || pos.x > 1 - _maxBoundary || pos.y < _maxBoundary || pos.y > 1 - _maxBoundary) {
+            
+            if (pos.x < _maxerBoundary || pos.x > 1 - _maxerBoundary || pos.y < _maxerBoundary || pos.y > 1 - _maxerBoundary) {
+                _camera.transform.localPosition = new Vector3(_camera.transform.localPosition.x, _camera.transform.localPosition.y, _camera.transform.localPosition.z - _cameraSpeedOuter);
+            }
+            else if (pos.x < _maxBoundary || pos.x > 1 - _maxBoundary || pos.y < _maxBoundary || pos.y > 1 - _maxBoundary) {
                 _camera.transform.localPosition = new Vector3(_camera.transform.localPosition.x, _camera.transform.localPosition.y, _camera.transform.localPosition.z - _cameraSpeedOut);
             }
-        }
 
-        var allIn = true;
-        foreach (GameObject player in _players) {
-            var pos = _camera.WorldToViewportPoint(player.transform.position);
+            if (_camera.transform.localPosition.z > _maxDistance) {
+                _camera.transform.localPosition = new Vector3(_camera.transform.localPosition.x, _camera.transform.localPosition.y, _maxDistance);
+            }
+
             if (pos.x < _minBoundary || pos.x > 1 - _minBoundary || pos.y < _minBoundary || pos.y > 1 - _minBoundary) {
                 allIn = false;
             }
+
+            totalVector += player.transform.position;
         }
+
+        totalVector = totalVector / _players.Count;
 
         if (allIn) {
             _camera.transform.localPosition = new Vector3(_camera.transform.localPosition.x, _camera.transform.localPosition.y, _camera.transform.localPosition.z + _cameraSpeedIn);
         }
+
+        _camera.transform.localPosition = Vector3.Lerp(_camera.transform.localPosition, new Vector3(totalVector.x, totalVector.y, _camera.transform.localPosition.z), 10f);
     }
 }
