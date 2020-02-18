90a46dfd04b7268da695082071901860aee65961
Lachezar Hubanov (SI) <lachezar.hubanov@sigames.com>
2020-02-18 00:35:14 +0000

    added stretching script + rope balance

---
+++ Assets/Scripts/StretchSound.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StretchSound : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject player1;
+
+	[SerializeField]
+	private GameObject player2;
+
+	[SerializeField]
+	private AudioSource source;
+
+	[SerializeField]
+	private List<AudioClip> stretchingClips;
+
+	[SerializeField]
+	private float cooldown;
+
+	private float nextTime;
+
+	private void Start()
+	{
+		source = GetComponent<AudioSource>();
+
+		nextTime = 0;
+	}
+
+	private void Update()
+    {
+        if( player1 && player2 )
+		{
+			if( Vector3.Distance( player1.transform.position, player2.transform.position ) > 15 && 
+				Time.time > nextTime )
+			{
+				if ( stretchingClips.Count > 0 )
+				{ 
+					System.Random rng = new System.Random();
+					int clipIndex = rng.Next(0, stretchingClips.Count - 1);
+					source.PlayOneShot(stretchingClips[clipIndex]);
+
+					nextTime = Time.time + cooldown;
+				}
+			}
+		}
+    }
+}
